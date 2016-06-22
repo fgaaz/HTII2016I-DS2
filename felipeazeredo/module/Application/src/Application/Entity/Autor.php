99bce3e03c1e359b69f239214a2a24169d97f68e
fgaaz <felipe.fgaaz@hotmail.com>
2016-06-21 22:42:38 -0300

    Trabalho Final DSII - 21/06/16

---
+++ felipeazeredo/module/Application/src/Application/Entity/Autor.php
@@ -0,0 +1,62 @@
+<?php
+namespace Application\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+	* @ORM\Entity
+	* @ORM\Table(name="autores")
+**/
+
+class Autor
+{
+	/**
+		* @ORM\Id
+		* @ORM\GeneratedValue(strategy="AUTO")
+		* @ORM\Column(type="integer")
+	**/
+	protected $id;
+	
+	/**
+		* @ORM\Column(type="string", length=100)
+	**/
+	protected $nome;
+	
+	/**
+		* @ORM\ManyToMany(targetEntity="Livro", mappedBy="autores")
+	**/
+	protected $livros;
+	
+	//Construtor
+	public function __construct()
+	{
+		$this->livros = new ArrayCollection();
+	}
+	
+	//Getters & Setters
+	public function addLivro(Livro $livro)
+	{
+		if (!$this->livros->contains($livro)){
+			$this->livros->add($livro);
+		}
+		return $this;
+	}
+	
+	public function removeLivro(Livro $livro)
+	{
+		if ($this->livros->contains($livro)){
+			$this->livros->removeElement($livro);
+		}
+		return $this;
+	}
+	
+	public function __get($name)
+	{
+		return $this->$name;
+	}
+	public function __set($name, $value)
+	{
+		$this->$name = $value;
+	}
+}
---
+++ felipeazeredo/module/Application/src/Application/Entity/Genero.php
@@ -0,0 +1,62 @@
+<?php
+namespace Application\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+	* @ORM\Entity
+	* @ORM\Table(name="generos")
+**/
+
+class Genero
+{
+	/**
+		* @ORM\Id
+		* @ORM\GeneratedValue(strategy="AUTO")
+		* @ORM\Column(type="integer")
+	**/
+	protected $id;
+	
+	/**
+		* @ORM\Column(type="string", length=50, unique=TRUE)
+	**/
+	protected $descricao;
+	
+	/**
+		* @ORM\OneToMany(targetEntity="Livro", mappedBy="genero")
+	**/
+	protected $livros;
+	
+	//Construtor
+	public function __construct()
+	{
+		$this->livros = new ArrayCollection();
+	}
+	
+	//Getters & Setters
+	public function addLivro(Livro $livro)
+	{
+		if (!$this->livros->contains($livro)){
+			$this->livros->add($livro);
+		}
+		return $this;
+	}
+	
+	public function removeLivro(Livro $livro)
+	{
+		if ($this->livros->contains($livro)){
+			$this->livros->removeElement($livro);
+		}
+		return $this;
+	}
+	
+	public function __get($name)
+	{
+		return $this->$name;
+	}
+	public function __set($name, $value)
+	{
+		$this->$name = $value;
+	}
+}
